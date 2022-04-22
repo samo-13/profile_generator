1ff100da40adc7c13b1e19d3baa789d27be4405b
Sarah Moss <sarah-moss@live.com>
2022-04-22 14:23:04 +0000

    Remove example code

--- survey.js
+++ survey.js
@@ -5,13 +5,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-});
-
-
 rl.question('What\'s your name? Nicknames are also acceptable: ', (answer1) => {
   console.log(`Thank you for your answer: ${answer1}`);
 
